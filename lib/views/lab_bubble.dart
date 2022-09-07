d16e46f09dbdbaea8e5ff801fd5178a537961f54
Aidan Dagnall <aidandagnall@gmail.com>
2022-09-07 21:24:32 +0100

    Add tooltip for module names

--- lib/views/lab_bubble.dart
+++ lib/views/lab_bubble.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lab_availability_checker/models/lab.dart';
-import 'package:lab_availability_checker/util/module_code_provider.dart';
+import 'package:lab_availability_checker/providers/module_code_provider.dart';
 import 'package:provider/provider.dart';
 
 class LabBubble extends StatelessWidget {
@@ -11,37 +11,41 @@ class LabBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Theme.of(context).colorScheme.primary,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-      child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-          child: Row(children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 5),
-              child: Icon(
-                lab!.getIcon(),
-                color: Theme.of(context).colorScheme.onPrimary,
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Consumer<ModuleCodeStyleProvider>(
-                    builder: (ctx, provider, child) => Text(
-                          lab!.module.getModuleCodeWithStyle(provider.style),
-                          style: GoogleFonts.openSans(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                              fontWeight: FontWeight.w700),
-                        )),
-                Text(
-                  lab!.getStartTime() + " - " + lab!.getEndTime(),
-                  style: GoogleFonts.openSans(
+        color: Theme.of(context).colorScheme.primary,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+        child: Tooltip(
+          message: lab!.module.name,
+          enableFeedback: true,
+          triggerMode: TooltipTriggerMode.tap,
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+              child: Row(children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 5),
+                  child: Icon(
+                    lab!.getIcon(),
                     color: Theme.of(context).colorScheme.onPrimary,
                   ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Consumer<ModuleCodeStyleProvider>(
+                        builder: (ctx, provider, child) => Text(
+                              lab!.module.getModuleCodeWithStyle(provider.style),
+                              style: GoogleFonts.openSans(
+                                  color: Theme.of(context).colorScheme.onPrimary,
+                                  fontWeight: FontWeight.w700),
+                            )),
+                    Text(
+                      lab!.getStartTime() + " - " + lab!.getEndTime(),
+                      style: GoogleFonts.openSans(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                      ),
+                    )
+                  ],
                 )
-              ],
-            )
-          ])),
-    );
+              ])),
+        ));
   }
 }
