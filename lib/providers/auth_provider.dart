805f29ba84f7601493815616ad8f9054ba4c5804
Aidan Dagnall <aidandagnall@gmail.com>
2022-10-07 00:18:32 +0100

    Update auth provider to handle login types, loading the last used one from preferences

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,35 +2,59 @@ import 'dart:io' show Platform;
 import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart';
 import 'package:lab_availability_checker/util/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum LoginType { user, admin }
 
 class AuthProvider extends ChangeNotifier {
   Credentials? credentials;
   late Auth0 auth0;
-  // Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
+  SharedPreferences? _prefs;
 
-  AuthProvider() {
-    getStoredCredentials();
+  AuthProvider(LoginType type) {
+    if (type == LoginType.user) {
+      auth0 = Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
+    } else if (type == LoginType.admin) {
+      auth0 = Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_ADMIN_CLIENT_ID']!);
+    }
+    getPrefs();
+  }
+
+  getPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
   }
 
   login() async {
-    auth0 = Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
     final _credentials = await auth0
         .webAuthentication(scheme: Platform.isAndroid ? "lab-monitor" : null)
         .login(audience: Constants.API_URL);
     credentials = _credentials;
+    _prefs!.setBool("login/is-admin", false);
     notifyListeners();
+  }
+
+  changeLoginType(LoginType type) async {
+    if (type == LoginType.user) {
+      auth0 = Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
+    } else if (type == LoginType.admin) {
+      auth0 = Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_ADMIN_CLIENT_ID']!);
+    }
   }
 
   loginWithPassword() async {
-    auth0 = Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_ADMIN_CLIENT_ID']!);
+    changeLoginType(LoginType.admin);
     final _credentials = await auth0
         .webAuthentication(scheme: Platform.isAndroid ? "lab-monitor" : null)
         .login(audience: Constants.API_URL);
     credentials = _credentials;
+    _prefs!.setBool("login/is-admin", true);
+
     notifyListeners();
   }
 
+  setLoginType() async {}
   updateCredentials() async {
     if (credentials!.expiresAt.compareTo(DateTime.now()) == -1) {
       credentials = await auth0.credentialsManager.credentials();
@@ -52,6 +76,7 @@ class AuthProvider extends ChangeNotifier {
     await auth0.webAuthentication(scheme: Platform.isAndroid ? "lab-monitor" : null).logout();
     await auth0.credentialsManager.clearCredentials();
     credentials = null;
+    _prefs!.remove("login/is-admin");
     notifyListeners();
   }
 }
